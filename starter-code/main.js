fd256be2bf1d9c44365ff18fccf9dac841d33fae
Nate <sauceynate@gmail.com>
2017-01-24 23:23:12 -0600

    added function compare

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,14 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+function compare(cardTwo, cardFour){
+	if ("queen" === "queen"){
+		lert("You found a match!");
+		}
+	else if ("king" === "king"){
+		alert("You found a match!");
+		}
+	else
+		alert("Sorry, not a match.");
+}
